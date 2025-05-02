fd85155c1296292e17beddd4cc7061524bccd5c4
WooKyoung Noh <wookay.noh@gmail.com>
2025-05-03 06:39:23 +0900

    test functors

---
+++ test/logicaloperators/functors.jl
@@ -0,0 +1,18 @@
+module test_logicaloperators_functors
+
+using Test
+using LogicalOperators: AND, OR
+
+and = AND{Function}(isone, isodd)
+@test all(f -> f(1), and.elements)
+
+and = AND{Function}(iszero, iseven)
+@test all(f -> f(0), and.elements)
+
+or = OR{Function}(isone, isodd)
+@test any(f -> f(1), or.elements)
+
+or = OR{Function}(iszero, iseven)
+@test any(f -> f(0), or.elements)
+
+end # module test_logicaloperators_functors
